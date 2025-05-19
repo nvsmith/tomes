fa8cf2580d6d3838152e293d6143e6b0e6e6396e
Nathan Smith <nathanielvs@gmail.com>
2025-05-19 10:05:01 -0600

    Add layout file to app. Import Stack component to handle navigation.

---
+++ app/_layout.jsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+const RootLayout = () => {
+    return (
+        <View style={{ flex: 1 }}>
+            {/* Render homepage */}
+            <Stack />
+            <Text>Footer</Text>
+        </View>
+    );
+};
+export default RootLayout;
+const styles = StyleSheet.create({});
