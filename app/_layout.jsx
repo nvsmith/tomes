0a3e8d9d2dea776c0041ad6ec38dfe639a3bdf48
Nathan Smith <nathanielvs@gmail.com>
2025-05-19 10:57:18 -0600

    Remove superfluous View element. Add global style options to the Stack and individual style options to each Screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,11 +2,16 @@ import { Stack } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 const RootLayout = () => {
     return (
-        <View style={{ flex: 1 }}>
-            {/* Render homepage */}
-            <Stack />
-            <Text>Footer</Text>
-        </View>
+        <Stack
+            screenOptions={{
+                headerStyle: { backgroundColor: "#ddd" },
+                headerTintColor: "#333",
+            }}
+        >
+            <Stack.Screen name="index" options={{ title: "Home" }} />
+            <Stack.Screen name="about" options={{ title: "About" }} />
+            <Stack.Screen name="contact" options={{ title: "Contact", headerShown: false }} />
+        </Stack>
     );
 };
 export default RootLayout;
