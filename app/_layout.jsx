f63052c04c23a53d26cf5d514a11d43aecc4aa41
Nathan Smith <nathanielvs@gmail.com>
2025-05-20 11:09:42 -0600

    Add StatusBar config to global theme. Nest layout template under a fragment.

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,22 +1,26 @@
 import { Stack } from "expo-router";
 import { StyleSheet, Text, useColorScheme, View } from "react-native";
 import { Colors } from "../constants/Colors";
+import { StatusBar } from "expo-status-bar";
 
 const RootLayout = () => {
     const colorScheme = useColorScheme();
     const theme = Colors[colorScheme] ?? Colors.light;
 
     return (
-        <Stack
-            screenOptions={{
-                headerStyle: { backgroundColor: theme.navBackground },
-                headerTintColor: theme.title,
-            }}
-        >
-            <Stack.Screen name="index" options={{ title: "Home" }} />
-            <Stack.Screen name="about" options={{ title: "About" }} />
-            <Stack.Screen name="contact" options={{ title: "Contact" }} />
-        </Stack>
+        <>
+            <StatusBar value="auto" />
+            <Stack
+                screenOptions={{
+                    headerStyle: { backgroundColor: theme.navBackground },
+                    headerTintColor: theme.title,
+                }}
+            >
+                <Stack.Screen name="index" options={{ title: "Home" }} />
+                <Stack.Screen name="about" options={{ title: "About" }} />
+                <Stack.Screen name="contact" options={{ title: "Contact" }} />
+            </Stack>
+        </>
     );
 };
 
