7ee500115a22666fb0f610492427e26c14a6d622
Nathan Smith <nathanielvs@gmail.com>
2025-05-20 10:42:33 -0600

    Add fallback global theme. Add theme config to About page.

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,20 +4,22 @@ import { Colors } from "../constants/Colors";
 
 const RootLayout = () => {
     const colorScheme = useColorScheme();
-    const theme = Colors[colorScheme];
+    const theme = Colors[colorScheme] ?? Colors.light;
 
     return (
         <Stack
             screenOptions={{
-                headerStyle: { backgroundColor: "#ddd" },
-                headerTintColor: "#333",
+                headerStyle: { backgroundColor: theme.navBackground },
+                headerTintColor: theme.title,
             }}
         >
             <Stack.Screen name="index" options={{ title: "Home" }} />
             <Stack.Screen name="about" options={{ title: "About" }} />
-            <Stack.Screen name="contact" options={{ title: "Contact", headerShown: false }} />
+            <Stack.Screen name="contact" options={{ title: "Contact" }} />
         </Stack>
     );
 };
+
 export default RootLayout;
+
 const styles = StyleSheet.create({});
