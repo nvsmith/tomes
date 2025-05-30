09f71aea7b18e71e25e2daf86764a7081b78be75
Nathan Smith <nathanielvs@gmail.com>
2025-05-30 12:14:36 -0600

    refactor: replace native components with themed components on About page

--- app/about.jsx
+++ app/about.jsx
@@ -1,19 +1,21 @@
-import { StyleSheet, Text, View, useColorScheme } from "react-native";
 import { Link } from "expo-router";
-import { Colors } from "../constants/Colors";
+import { StyleSheet } from "react-native";
+
+// Themed Components
+import ThemedView from "../components/ThemedView";
+import ThemedText from "../components/ThemedText";
 
 const About = () => {
-    const colorScheme = useColorScheme();
-    const theme = Colors[colorScheme] ?? Colors.light;
-
     return (
-        <View style={[styles.container, { backgroundColor: theme.background }]}>
-            <Text style={styles.title}>About</Text>
+        <ThemedView style={styles.container}>
+            <ThemedText style={styles.title} title={true}>
+                About
+            </ThemedText>
 
             <Link href="/" style={styles.link}>
-                Back Home
+                <ThemedText>Back Home</ThemedText>
             </Link>
-        </View>
+        </ThemedView>
     );
 };
 
