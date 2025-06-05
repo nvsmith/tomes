9166bde11a6241c2400ca4052fa5c381acbfad46
Nathan Smith <nathanielvs@gmail.com>
2025-06-05 11:55:28 -0600

    feat(auth): create register page and update links on homepage to the register and login pages. Delete About and Contact pages.

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -6,24 +6,24 @@ import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
 
-const Login = () => {
+const Register = () => {
     return (
         <ThemedView style={styles.container}>
             <Spacer />
 
             <ThemedText title={true} style={styles.title}>
-                Login to Your Account
+                Register For An Account
             </ThemedText>
 
             <Spacer height={100} />
 
-            <Link href="/register" style={{ textAlign: "center" }}>
-                <ThemedText>Register instead</ThemedText>
+            <Link href="/login" style={{ textAlign: "center" }}>
+                <ThemedText>Login Instead</ThemedText>
             </Link>
         </ThemedView>
     );
 };
-export default Login;
+export default Register;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
