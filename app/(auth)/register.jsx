4d96a84588f19d91fc8af763e3a9ea6b69a06479
Nathan Smith <nathanielvs@gmail.com>
2025-06-11 11:08:04 -0600

    feat: add ThemedButton to register screen.

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,12 +1,17 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { Link } from "expo-router";
 
 // Themed Components
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
+import ThemedButton from "../../components/ThemedButton";
 import Spacer from "../../components/Spacer";
 
 const Register = () => {
+    const handleSubmit = () => {
+        console.log("register form submitted");
+    };
+
     return (
         <ThemedView style={styles.container}>
             <Spacer />
@@ -14,6 +19,10 @@ const Register = () => {
             <ThemedText title={true} style={styles.title}>
                 Register For An Account
             </ThemedText>
+
+            <ThemedButton onPress={handleSubmit}>
+                <Text style={{ color: "#f2f2f2" }}>Register</Text>
+            </ThemedButton>
 
             <Spacer height={100} />
 
@@ -30,6 +39,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
+        alignItems: "center",
     },
     title: {
         textAlign: "center",
