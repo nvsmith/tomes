57275f836f6b0c5172e9a80e882e9845f9c77973
Nathan Smith <nathanielvs@gmail.com>
2025-06-17 08:54:40 -0600

    feature: add create page where user can add new book

---
+++ app/(dashboard)/create.jsx
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+import Spacer from "../../components/Spacer";
+import ThemedText from "../../components/ThemedText";
+import ThemedView from "../../components/ThemedView";
+
+const Create = () => {
+    return (
+        <ThemedView style={styles.container}>
+            <ThemedText title={true} style={styles.heading}>
+                Add A New Book
+            </ThemedText>
+        </ThemedView>
+    );
+};
+
+export default Create;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    heading: {
+        fontWeight: "bold",
+        fontSize: 18,
+        textAlign: "center",
+    },
+});
